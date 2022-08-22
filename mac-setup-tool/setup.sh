453e2d94e2ad27f944afce333f39ec275071f51c
tlin-taolin <itamtao@gmail.com>
2022-08-22 15:47:56 +0200

    minor (also commit something old)

--- mac-setup-tool/setup.sh
+++ mac-setup-tool/setup.sh
@@ -92,6 +92,11 @@ echo '
 brew install markdown
 brew install ant
 brew install ack
+brew install perl
+PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
+echo '
+# set perl
+eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"' >> ~/.zshrc
 
 prompt "Install brew software."
 brew install --cask iterm2
@@ -144,6 +149,15 @@ cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
+
+
+prompt "configure latexindent"
+sudo cpan Unicode::GCString
+sudo cpan App::cpanminus
+sudo cpan YAML::Tiny
+sudo perl -MCPAN -e 'install "File::HomeDir"'
+
+
 
 
 prompt "Cleanup"

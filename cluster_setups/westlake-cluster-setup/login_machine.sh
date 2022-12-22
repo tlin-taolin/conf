11f066c41fe1e3e1881e676cc0a11592b847c483
tlin-taolin <itamtao@gmail.com>
2022-12-22 07:40:12 +0000

    minor for new machine script.

--- cluster_setups/westlake-cluster-setup/login_machine.sh
+++ cluster_setups/westlake-cluster-setup/login_machine.sh
@@ -3,3 +3,17 @@ cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
+
+# install my vim
+curl https://j.mp/spf13-vim3 -L -o - | sh
+
+# install autojump
+cd
+git clone git@github.com:wting/autojump.git
+cd autojump
+python3 ./install.py
+echo '
+# autojump
+[[ -s /home/taolin/.autojump/etc/profile.d/autojump.sh ]] && source /home/taolin/.autojump/etc/profile.d/autojump.sh' >> ~/.bashrc
+cd ..
+rm -rf autojump
